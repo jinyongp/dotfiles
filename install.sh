b0032f5977cb058f48135726ee9087224da4817c
jinyongp <dev.jinyongp@gmail.com>
2022-05-28 15:50:12 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -14,13 +14,12 @@ dotfiles=(
 
 echo "Installing dotfiles..."
 for file in ${dotfiles[*]}; do
-  overwrite=false
   if [[ -f $HOME/$file ]]; then
     rm $HOME/$file
     overwrite=true
   fi
   ln -s $CWD/$file $HOME/$file
-  echo "\t$file linked. $([[ $overwrite == true ]] && echo '(overwritten)')"
+  echo -e "\t$file linked. $([[ $overwrite == true ]] && echo '(overwritten)')"
 done
 echo "Done."
 
@@ -28,6 +27,6 @@ GIT_HOOKS_DIR=$HOME/.git-template/hooks
 if [[ ! -d "$GIT_HOOKS_DIR" ]]; then
   echo "\nGit hooks directory not found. Creating..."
   mkdir -p $GIT_HOOKS_DIR
-  git clone https://gist.github.com/d8a4ce41e4bb52e352d45306691e3122.git ~/.git-template/hooks --quiet
+  git clone https://gist.github.com/d8a4ce41e4bb52e352d45306691e3122.git $GIT_HOOKS_DIR --quiet
   echo "Git hooks installed. Check it out $GIT_HOOKS_DIR"
 fi
