6b1420dd875b264cfb65540295a93764807bb715
jinyongp <dev.jinyongp@gmail.com>
2022-06-16 23:56:24 +0900

    DefaultkeyBindings 추가

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ dotfiles=(
   .gitconfig_company
 )
 
-echo -e "Installing dotfiles...\n"
+echo -e "Installing dotfiles..."
 
 overwritten=()
 linked=()
@@ -43,23 +43,28 @@ done
   printf "  %s\n" ${overwritten[@]}
 )
 
-echo
-
 [[ ${#linked[@]} > 0 ]] && (
   echo "New linked:"
   printf "  %s\n" ${linked[@]}
 )
 
-[[ ${#overwritten[@]} > 0 ]] && echo -e "\nYour original files are backed up in $BACKUP_DIR\n"
+[[ ${#overwritten[@]} > 0 ]] && echo -e "\nYour original files are backed up in $BACKUP_DIR"
 
 echo -e "All dotfiles installed. Enjoy! :)"
 
 GIT_HOOKS_DIR=$HOME/.git-template/hooks
 if [[ ! -d "$GIT_HOOKS_DIR" ]]; then
-  echo -e -e "\nGit hooks not found. Installing..."
+  echo -e "Git hooks not found. Installing..."
   mkdir -p $GIT_HOOKS_DIR
   git clone https://gist.github.com/d8a4ce41e4bb52e352d45306691e3122.git $GIT_HOOKS_DIR --quiet
   echo -e "Git hooks installed. Check it out $GIT_HOOKS_DIR"
 fi
+
+KEY_BINDINGS_DIR=$HOME/Library/KeyBindings
+DEFAULT_KEY_BINDING_FILE="DefaultkeyBinding.dict"
+ORIGINAL_KEY_BINDING_FILE=$KEY_BINDINGS_DIR/$DEFAULT_KEY_BINDING_FILE
+[[ -f $ORIGINAL_KEY_BINDING_FILE ]] && cp $ORIGINAL_KEY_BINDING_FILE $BACKUP_DIR/$DEFAULT_KEY_BINDING_FILE
+mkdir -p $KEY_BINDINGS_DIR && ln -sf $CWD/dict/$DEFAULT_KEY_BINDING_FILE $ORIGINAL_KEY_BINDING_FILE
+echo -e "\nDefault key bindings installed. Check it out $ORIGINAL_KEY_BINDING_FILE"
 
 echo -e "\nFollow the instructions in README.md to complete the installation."
