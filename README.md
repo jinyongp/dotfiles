38e032a087fe89e3fdc9598846537dbd20655821
jinyongp <dev.jinyongp@gmail.com>
2022-09-14 23:54:40 +0900

    모든 패키지를 한번에 설치하도록 개선

--- README.md
+++ README.md
@@ -1,42 +1,70 @@
-# My Custom Dotfiles
+# My Dotfiles
 
-## Installation
+##
 
-First, clone this repository in `$HOME` directory.
+Clone this repository.
 
 ```sh
 $ git clone https://github.com/jinyongp/dotfiles.git ~/dotfiles
 ```
 
-Run `install.sh` to copy(symlink) all of the dotfiles in home directory.
+Run `install.sh` to install all packages and plugins, and copy(symlink) all of the dotfiles in home directory.
 
 ```sh
 $ ~/dotfiles/install.sh
 ```
 
->The files that already existed are backed up in `dotfiles/.backup` directory.
+> The files that already existed are backed up in `.backup` directory.
 
-## Setup
+## Installed packages and plugins
+
+### Homebrew
+
+[Install Homebrew](https://brew.sh/index_ko) and some packages
+
+- Formulae
+
+  ```
+  brew install jq bat direnv gnupg exa gh node
+  ```
+
+- Casks
+
+  ```
+  brew install --cask iterm2 miniconda karabiner-elements raycast keka kekaexternalhelper google-chrome macvim visual-studio-code
+  ```
 
 ### Oh My Zsh
 
-[Install Oh My Zsh](https://ohmyz.sh/#install)
+[Install Oh My Zsh](https://ohmyz.sh/#install) and some plugins as below
 
-Install external plugins for oh-my-zsh.
+- [alias-tips](https://github.com/djui/alias-tips#oh-my-zsh)
+- [zsh-completions](https://github.com/zsh-users/zsh-completions#oh-my-zsh)
+- [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh)
+- [fast-syntax-highlighting](https://github.com/zdharma-continuum/fast-syntax-highlighting#oh-my-zsh)
+- [zsh-better-npm-completion](https://github.com/lukechilds/zsh-better-npm-completion#as-an-oh-my-zsh-custom-plugin)
+- [conda-zsh-completion](https://github.com/esc/conda-zsh-completion/blob/382d840f7ad053b3b2ccf0b1f52b26bdabaf66b3/_conda#L24)
 
-   1. [alias-tips](https://github.com/djui/alias-tips#oh-my-zsh)
-   2. [direnv](https://github.com/direnv/direnv/blob/master/docs/installation.md#installation)
-   3. [zsh-autosuggestions](https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md#oh-my-zsh)
-   4. [zsh-completions](https://github.com/zsh-users/zsh-completions#oh-my-zsh)
-   5. [fast-syntax-highlighting](https://github.com/zdharma-continuum/fast-syntax-highlighting#oh-my-zsh)
-   6. [conda-zsh-completion](https://github.com/esc/conda-zsh-completion/blob/382d840f7ad053b3b2ccf0b1f52b26bdabaf66b3/_conda#L24)
-   7. [zsh-better-npm-completion](https://github.com/lukechilds/zsh-better-npm-completion#as-an-oh-my-zsh-custom-plugin)
+And, install [PowerLevel10K](https://github.com/romkatv/powerlevel10k#oh-my-zsh) Theme!
 
-And, install [PowerLevel10K](https://github.com/romkatv/powerlevel10k#oh-my-zsh) Theme! (I'm using [Fira Code Nerd Font](https://github.com/ryanoasis/nerd-fonts#option-4-homebrew-fonts).)
-
-Run command `$ omz reload` to load `~/.zshrc`.
+> Run command `$ omz reload` to load `~/.zshrc`.
 
 ### VundleVim
 
 - [Install VundleVim](https://github.com/VundleVim/Vundle.vim#quick-start)
-- Run command [`$ vundle`](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/vundle) to install plugins.
+- Run command [`$ vundle`](https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/vundle) to install vundlevim plugins.
+
+### Assets
+
+- [Install nerd-font](https://github.com/ryanoasis/nerd-fonts#option-4-homebrew-fonts) using homebrew
+
+  ```sh
+  $ brew tap homebrew/cask-fonts
+  $ brew install --cask font-fira-code-nerd-font
+  ```
+
+- Copy the fonts to `~/Library/Fonts` (It is not working. Just example.)
+
+  ```sh
+  $ cp [/assets/fonts/*] ~/Library/Fonts/*
+  ```
--- install.sh
+++ install.sh
@@ -1,6 +1,103 @@
-#!/bin/bash
+#!/bin/zsh
 
-CWD=$(dirname $(readlink -f $0))
+CWD=$(cd "$(dirname "$0")" && pwd)
+
+reset='\033[0m'
+green() { printf "\033[0;32m$*$reset" }
+cyan() { printf "\033[0;36m$*$reset" }
+red() { printf "\033[0;31m$*$reset" }
+
+if ! type brew &>/dev/null; then
+  echo
+  echo -ne "$(green Installing homebrew...)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo -e "$(green Done)"
+fi
+
+if type brew &>/dev/null; then
+  echo -e "$(green Installing homebrew packages...)"
+  local formulaes=(jq bat direnv gnupg exa gh node)
+  for formula in ${formulaes[*]}; do
+    echo -ne "Installing $formula... "
+    if brew list $formula &>/dev/null; then
+      echo -e "$(cyan Already installed)"
+    else
+      brew install $formula
+      echo -e "$(green Done)"
+    fi
+  done
+
+  local casks=(iterm2 miniconda karabiner-elements raycast keka kekaexternalhelper google-chrome macvim visual-studio-code)
+  for cask in ${casks[*]}; do
+    echo -ne "Installing $cask... "
+    if brew list $cask &>/dev/null; then
+      echo -e "$(cyan Already installed)"
+    else
+      brew install --cask $cask &>/dev/null
+      echo -e "$(green Done)"
+    fi
+  done
+  echo -e "$(green Installing homebrew packages... Done)\n"
+  echo -e "$(cyan Installed homebrew packages:)"
+  brew list
+  echo
+fi
+
+if type brew &>/dev/null; then
+  echo -ne "$(green Installing fonts... )"
+  nerd_font="font-fira-code-nerd-font"
+  brew list $nerd_font &>/dev/null || {
+    brew tap homebrew/cask-fonts &>/dev/null
+    brew install --cask $nerd_font &>/dev/null
+  }
+  IFS=$'\n' && internal_fonts=($(find $CWD/assets/fonts -name '*.?tf'))
+  for font in ${internal_fonts[*]}; do
+    font_name=$(basename $font)
+    if [[ ! -f $HOME/Library/Fonts/$font_name ]]; then
+      cp $font $HOME/Library/Fonts/$font_name
+    fi
+  done
+  echo -e "$(green Done)"
+fi
+
+if [[ ! -d "$ZSH" ]]; then
+  echo
+  echo -ne "$(green "Installing oh-my-zsh... ")"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &>/dev/null & wait
+  echo -e "$(green Done)"
+fi
+
+if [[ -d "$ZSH" ]]; then
+  echo -ne "$(green "Installing powerlevel10k theme... ")"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k &>/dev/null & wait
+  echo -e "$(green Done)"
+
+  echo -ne "$(green "Installing vundlevim... ")"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  echo -e "$(green Done)"
+
+  echo -e "$(green "Installing oh-my-zsh plugins... ")"
+  local PLUGINS_DIR="${ZSH_CUSTOM:=$HOME/.oh-my-zsh/custom}/plugins"
+  local plugins=(
+    djui/alias-tips
+    zsh-users/zsh-completions
+    zsh-users/zsh-autosuggestions
+    esc/conda-zsh-completion
+    lukechilds/zsh-better-npm-completion
+    zdharma-continuum/fast-syntax-highlighting
+  )
+  for plugin in ${plugins[*]}; do
+    local plugin_name=$(basename $plugin)
+    echo -ne "Installing $plugin_name... "
+    if [[ -d "$PLUGINS_DIR/$plugin_name" ]]; then
+      echo -e "$(cyan Already installed)"
+    else
+      git clone "https://github.com/$plugin" "$PLUGINS_DIR/$plugin_name" &>/dev/null
+      echo -e "$(green Done)"
+  done
+  echo -e "$(green "Installing oh-my-zsh plugins... Done")"
+  echo
+fi
 
 dotfiles=(
   .zshrc
@@ -15,7 +112,7 @@ dotfiles=(
   .gitconfig_company
 )
 
-echo -e "Installing dotfiles...\n"
+echo -e "$(green Installing dotfiles...)\n"
 
 overwritten=()
 linked=()
@@ -50,8 +147,9 @@ done
 
 echo
 [[ ${#overwritten[@]} > 0 ]] && echo -e "Backup files in $BACKUP_DIR"
-echo -e "\nAll dotfiles installed!"
-echo -e "Need to restart your terminal to apply changes.\n"
+echo -e "\n$(cyan All dotfiles installed!!)"
+echo -e "Run $(green $ omz reload) to reload oh-my-zsh."
+echo -e "Run $(green $ vundle) to install vim plugins."
 
 GIT_TEMPLATE_DIR=$HOME/.git_template
 GIT_HOOKS_DIR=$GIT_TEMPLATE_DIR/hooks
