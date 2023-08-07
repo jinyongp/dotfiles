1d362b798f261e4c6d8790e4f7d05c86a89bbc3f
jinyongp <dev.jinyongp@gmail.com>
2023-08-07 12:52:57 +0900

    필요없는 설정 제거

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -11,10 +11,6 @@ export LANG=en_US.UTF-8
 # Custom Scripts
 export PATH=$HOME/.scripts:$PATH
 
-# NVM
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 # Brew
 if type brew &>/dev/null; then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
@@ -28,7 +24,7 @@ fi
 
 # deno
 if type deno &>/dev/null; then
-  deno completions zsh > "$(brew --prefix)/share/zsh/site-functions/_deno"
+  deno completions zsh >"$(brew --prefix)/share/zsh/site-functions/_deno"
 fi
 
 # pyenv
@@ -37,22 +33,3 @@ if type pyenv &>/dev/null; then
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
-
-# tabtab source for packages
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-miniconda="$(brew --prefix)/Caskroom/miniconda"
-__conda_setup="$('$miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-  eval "$__conda_setup"
-else
-  if [ -f "$miniconda/base/etc/profile.d/conda.sh" ]; then
-    . "$miniconda/base/etc/profile.d/conda.sh"
-  else
-    export PATH="$miniconda/base/bin:$PATH"
-  fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
