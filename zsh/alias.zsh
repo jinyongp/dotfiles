f067b700e0b27e7c165a56079be5e509511a8089
jinyongp <dev.jinyongp@gmail.com>
2023-10-27 04:09:55 +0900

    테마 및 패키지 설정 정리

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,31 +1,20 @@
+# python
 alias python=python3
 alias pip=pip3
 
-#alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-
-# Neovim
-alias vim='mvim -v'
-alias vi='mvim -v'
-
-# EZA
+# eza
 alias ls=ols
 alias ls="eza --grid --classify --icons --color=automatic --header --sort=type --git"
 alias lsd="ls --only-dirs"
 alias la="ls --all --long"
 alias lst="ls --reverse --tree"
 
-# Bat
-alias bat="bat --theme base16"
-
-# Git
+# git
 alias gopen="git open"
 alias gpft="git push --follow-tags"
 
-# Npm
+# npm
 alias npv="npm --no-git-tag-version version"
 
-# Xcode
+# xcode
 alias xcode="open -a Xcode"
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,38 +1,54 @@
-# Editor (https://github.com/macvim-dev/macvim)
-export EDITOR=$(which mvim)
+export EDITOR=$(which vim)
 
-# Manpage (https://github.com/sharkdp/bat)
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+export BAT_THEME="Nord"
 
-# Language
+# language
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# Custom Scripts
+# custom scripts
 export PATH=$HOME/.scripts:$PATH
 
-# Brew
-if type brew &>/dev/null; then
-  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-  autoload -Uz compinit && compinit
-fi
+function exists() {
+  type "$1" &>/dev/null
+}
 
 # ngrok
-if type ngrok &>/dev/null; then
+if exists ngrok; then
   source <(ngrok completion)
 fi
 
 # deno
-if type deno &>/dev/null; then
+if exists deno; then
   deno completions zsh >"$(brew --prefix)/share/zsh/site-functions/_deno"
 fi
 
 # pyenv
-if type pyenv &>/dev/null; then
+if exists pyenv; then
   export PYENV_ROOT="$HOME/.pyenv"
   command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
 
 # fnm
-eval "$(fnm env --use-on-cd)"
+if exists fnm; then
+  eval "$(fnm env --use-on-cd)"
+fi
+
+# bun
+if exists bun; then
+  source "$HOME/.bun/_bun"
+fi
+
+# poetry
+if exists poetry; then
+  poetry completions zsh >"$(brew --prefix)/share/zsh/site-functions/_poetry"
+fi
+
+# brew
+if exists brew; then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+  autoload -Uz compinit && compinit
+fi
+
+unset -f exists
