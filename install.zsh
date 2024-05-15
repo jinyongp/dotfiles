0a7d1217866e2ccf9a09cd74941e3517dfa0bd88
jinyongp <dev.jinyongp@gmail.com>
2024-05-16 05:42:29 +0900

    install script 오류 수정

--- install.zsh
+++ install.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-cd $(dirname $0)/scripts
+SCRIPTS=$(dirname $0)/scripts
 
-source ./install_homebrew.zsh
-source ./install_fonts.zsh
-source ./install_ohmyzsh.zsh
-source ./install_vundlevim.zsh
-source ./install_dotfiles.zsh
-source ./setup_macos.zsh
+source "${SCRIPTS}/install_homebrew.zsh"
+source "${SCRIPTS}/install_fonts.zsh"
+source "${SCRIPTS}/install_ohmyzsh.zsh"
+source "${SCRIPTS}/install_vundlevim.zsh"
+source "${SCRIPTS}/install_dotfiles.zsh"
+source "${SCRIPTS}/setup_macos.zsh"
 
 echo "🎉 Installation complete! Enjoy!"
