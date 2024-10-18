06772111be3edc9d1a17656e11a8e0132577d5b3
jinyongp <dev.jinyongp@gmail.com>
2024-10-19 00:38:47 +0900

    Android 관련 환경 변수 추가

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,9 @@
 export EDITOR=$(which vim)
 
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
 if exists brew; then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
   autoload -Uz compinit; compinit
