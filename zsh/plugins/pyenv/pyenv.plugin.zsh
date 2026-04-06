28621edb68e1a76e9e4971674437e376e050bf5a
jinyongp <dev.jinyongp@gmail.com>
2026-04-06 15:42:15 +0900

    pyenv plugin

--- zsh/plugins/pyenv/pyenv.plugin.zsh
+++ zsh/plugins/pyenv/pyenv.plugin.zsh
@@ -5,3 +5,8 @@ fi
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+
+python --version &>/dev/null
+if [[ $? -eq 127 ]] && (( $+commands[python3] )); then
+  alias python=python3
+fi
