569a1fccd40621f6fea00ea3979b99a37dce7061
jinyongp <dev.jinyongp@gmail.com>
2026-04-23 00:16:49 +0900

    cli 유틸 및 색상 helper 개선

--- utils/colors.zsh
+++ utils/colors.zsh
@@ -1,64 +1,64 @@
-#!/bin/zsh
+#!/usr/bin/env bash
 
-# Reset
-reset="\033[0m"
+dotfiles_color_reset=$'\033[0m'
 
-# Foreground
-black      (){ printf "\033[0;30m$*$reset"; }
-red        (){ printf "\033[0;31m$*$reset"; }
-green      (){ printf "\033[0;32m$*$reset"; }
-yellow     (){ printf "\033[0;33m$*$reset"; }
-blue       (){ printf "\033[0;34m$*$reset"; }
-purple     (){ printf "\033[0;35m$*$reset"; }
-cyan       (){ printf "\033[0;36m$*$reset"; }
-white      (){ printf "\033[0;37m$*$reset"; }
+dotfiles__color_print() {
+  local color_code="$1"
+  shift || true
 
-# Bold
-b_black    (){ printf "\033[1;30m$*$reset"; }
-b_red      (){ printf "\033[1;31m$*$reset"; }
-b_green    (){ printf "\033[1;32m$*$reset"; }
-b_yellow   (){ printf "\033[1;33m$*$reset"; }
-b_blue     (){ printf "\033[1;34m$*$reset"; }
-b_purple   (){ printf "\033[1;35m$*$reset"; }
-b_cyan     (){ printf "\033[1;36m$*$reset"; }
-b_white    (){ printf "\033[1;37m$*$reset"; }
+  printf '\033[%sm%s%s' "$color_code" "$*" "$dotfiles_color_reset"
+}
 
-# Transparent
-t_black    (){ printf "\033[2;30m$*$reset"; }
-t_red      (){ printf "\033[2;31m$*$reset"; }
-t_green    (){ printf "\033[2;32m$*$reset"; }
-t_yellow   (){ printf "\033[2;33m$*$reset"; }
-t_blue     (){ printf "\033[2;34m$*$reset"; }
-t_purple   (){ printf "\033[2;35m$*$reset"; }
-t_cyan     (){ printf "\033[2;36m$*$reset"; }
-t_white    (){ printf "\033[2;37m$*$reset"; }
-
-# Italic
-i_black    (){ printf "\033[3;30m$*$reset"; }
-i_red      (){ printf "\033[3;31m$*$reset"; }
-i_green    (){ printf "\033[3;32m$*$reset"; }
-i_yellow   (){ printf "\033[3;33m$*$reset"; }
-i_blue     (){ printf "\033[3;34m$*$reset"; }
-i_purple   (){ printf "\033[3;35m$*$reset"; }
-i_cyan     (){ printf "\033[3;36m$*$reset"; }
-i_white    (){ printf "\033[3;37m$*$reset"; }
-
-# Underline
-u_black    (){ printf "\033[4;30m$*$reset"; }
-u_red      (){ printf "\033[4;31m$*$reset"; }
-u_green    (){ printf "\033[4;32m$*$reset"; }
-u_yellow   (){ printf "\033[4;33m$*$reset"; }
-u_blue     (){ printf "\033[4;34m$*$reset"; }
-u_purple   (){ printf "\033[4;35m$*$reset"; }
-u_cyan     (){ printf "\033[4;36m$*$reset"; }
-u_white    (){ printf "\033[4;37m$*$reset"; }
-
-# Background
-bg_black   (){ printf "\033[40m$*$reset"; }
-bg_red     (){ printf "\033[41m$*$reset"; }
-bg_green   (){ printf "\033[42m$*$reset"; }
-bg_yellow  (){ printf "\033[43m$*$reset"; }
-bg_blue    (){ printf "\033[44m$*$reset"; }
-bg_purple  (){ printf "\033[45m$*$reset"; }
-bg_cyan    (){ printf "\033[46m$*$reset"; }
-bg_white   (){ printf "\033[47m$*$reset"; }
+while read -r color_name color_code; do
+  [[ -n "$color_name" ]] || continue
+  eval "${color_name}() { dotfiles__color_print '${color_code}' \"\$*\"; }"
+done <<'EOF'
+black 0;30
+red 0;31
+green 0;32
+yellow 0;33
+blue 0;34
+purple 0;35
+cyan 0;36
+white 0;37
+b_black 1;30
+b_red 1;31
+b_green 1;32
+b_yellow 1;33
+b_blue 1;34
+b_purple 1;35
+b_cyan 1;36
+b_white 1;37
+t_black 2;30
+t_red 2;31
+t_green 2;32
+t_yellow 2;33
+t_blue 2;34
+t_purple 2;35
+t_cyan 2;36
+t_white 2;37
+i_black 3;30
+i_red 3;31
+i_green 3;32
+i_yellow 3;33
+i_blue 3;34
+i_purple 3;35
+i_cyan 3;36
+i_white 3;37
+u_black 4;30
+u_red 4;31
+u_green 4;32
+u_yellow 4;33
+u_blue 4;34
+u_purple 4;35
+u_cyan 4;36
+u_white 4;37
+bg_black 40
+bg_red 41
+bg_green 42
+bg_yellow 43
+bg_blue 44
+bg_purple 45
+bg_cyan 46
+bg_white 47
+EOF
